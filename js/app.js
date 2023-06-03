bb758f1349eced7643c039c85d0d90bbfa1ac9cf
MartinezFabian <FabianMartinezDev@gmail.com>
2023-06-02 21:11:02 -0300

    feat: Add functionality to validate inputs and display alerts

--- js/app.js
+++ js/app.js
@@ -1,3 +1,57 @@
+// clases
+
+class AppointmentManager {
+  #appointments;
+
+  constructor() {
+    this.#appointments = [];
+  }
+
+  get appointments() {
+    return this.#appointments;
+  }
+}
+
+class UserInterface {
+  static showAlert(message, type) {
+    // comprobar si ya existe una alerta actualmente
+    const alertExist = document.querySelector(".form__alert");
+
+    if (alertExist) {
+      return;
+    }
+
+    // crear alerta
+    const alert = document.createElement("div");
+    alert.classList.add("form__alert");
+
+    if (type === "error") {
+      alert.classList.add("form__alert--error");
+    } else {
+      alert.classList.add("form__alert--success");
+    }
+
+    //crear el texto de la alerta
+    const alertText = document.createElement("P");
+    alertText.textContent = message;
+    alertText.classList.add("form__alert-text");
+
+    //agregar el texto a la alerta
+    alert.appendChild(alertText);
+
+    //obtener el contenedor
+    const container = document.querySelector(".main__patient");
+
+    //agregar la alerta al contenedor
+    container.appendChild(alert);
+
+    //eliminar la alerta luego de 2 segundos
+    setTimeout(() => {
+      alert.remove();
+    }, 2000);
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   main();
 });
@@ -38,11 +92,36 @@ function main() {
     inputAppointmentTime.addEventListener("input", readInputs);
     inputPatientSymptoms.addEventListener("input", readInputs);
     inputPatientDiagnosis.addEventListener("input", readInputs);
+    form.addEventListener("submit", addAppointment);
   }
 
+  // leer valores ingresados en los inputs y guardarlos en el objeto appointment
   function readInputs(e) {
     appointment[e.target.name] = e.target.value;
+  }
 
-    console.log(appointment);
+  // validar y agregar una nueva cita
+  function addAppointment(e) {
+    e.preventDefault();
+
+    const { patientName, patientAge, patientPhone, date, time, patientSymptoms, patientDiagnosis } =
+      appointment;
+
+    // validar
+
+    if (
+      patientName === "" ||
+      patientAge === "" ||
+      patientPhone === "" ||
+      date === "" ||
+      time === "" ||
+      patientSymptoms === ""
+    ) {
+      UserInterface.showAlert("Debes rellenar todos los campos obligatorios", "error");
+      return;
+    } else if (isNaN(patientPhone) || patientPhone.length < 7) {
+      UserInterface.showAlert("El número de teléfono ingresado no es válido", "error");
+      return;
+    }
   }
 }
